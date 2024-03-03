77baeaca85365a82c21b3f8b3cea249bdacb8f69
amekusa <hello@amekusa.com>
2024-03-04 05:35:53 +0900

    [chore] rollup: add a note

--- rollup.js
+++ rollup.js
@@ -10,7 +10,7 @@ import {env} from 'node:process';
 const output = {
 	sourcemap: env.NODE_ENV != 'production',
 	indent: false,
-	exports: 'named',
+	exports: 'named', // this is necessary if the entry point is using named and default exports together
 };
 
 export default {

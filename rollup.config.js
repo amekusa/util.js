1d781eadc16bbe3ed74e00f14e9713a407a80f4b
Satoshi Soma <noreply@amekusa.com>
2025-12-22 10:50:00 +0900

    rollup: overhaul

--- rollup.config.js
+++ rollup.config.js
@@ -6,27 +6,80 @@
  */
 
 import {env} from 'node:process';
+import terser from '@rollup/plugin-terser';
+
+const name = 'amekusa.util';
+
 const prod = env.NODE_ENV == 'production';
-
+const treeshake = prod; // treeshake options
 const output = {
+	name,
+	exports: 'named',
 	sourcemap: !prod,
-	indent: false,
-	exports: 'named', // this is necessary if the entry point has named and default exports together
+	compact: prod,
+	indent: !prod,
 };
 
-export default {
-	input: 'src/main.js',
-	output: [
-		{
-			file: 'dist/bundle.js',
-			format: 'es',
-			...output
-		},
-		{
-			file: 'dist/bundle.cjs',
-			format: 'cjs',
-			...output
-		},
-	],
-	treeshake: prod,
-};
+const tasks = [
+	{
+		// for node
+		input: 'src/main.js',
+		treeshake,
+		output: [
+			{
+				file: `dist/${name}.js`,
+				format: 'es',
+				...output
+			},
+			{
+				file: `dist/${name}.cjs`,
+				format: 'cjs',
+				...output
+			},
+		],
+	},
+	{
+		// for browser
+		input: 'src/main.br.js',
+		treeshake,
+		output: [
+			{
+				// for <script>
+				file: `dist/${name}.br.js`,
+				format: 'iife',
+				...output
+			},
+			{
+				// for <script type="module">
+				file: `dist/${name}.br.es.js`,
+				format: 'es',
+				...output
+			},
+		],
+	},
+];
+
+if (prod) {
+	tasks.push({
+		// minifiled versions
+		input: 'src/main.br.js',
+		treeshake,
+		output: [
+			{
+				file: `dist/${name}.br.min.js`,
+				format: 'iife',
+				...output
+			},
+			{
+				file: `dist/${name}.br.es.min.js`,
+				format: 'es',
+				...output
+			},
+		],
+		plugins: [
+			terser()
+		],
+	});
+}
+
+export default tasks;
